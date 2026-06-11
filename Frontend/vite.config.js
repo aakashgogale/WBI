1348740ae38d82556a810239134160ced7656b35
Aakash Gogale <aakashgogale123@gmail.com>
2026-06-11 16:04:39 +0530

    vercel bug solve

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -43,9 +43,6 @@ export default defineConfig({
       output: {
         manualChunks: (id) => {
           if (id.includes('node_modules')) {
-            if (id.includes('react/') || id.includes('react-dom/') || id.includes('react-router-dom/')) {
-              return 'react-core';
-            }
             if (id.includes('firebase/')) {
               return 'firebase';
             }
